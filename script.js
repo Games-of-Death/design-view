a1ae6de754060fbd8030d38926d441bf6c336933
Mandar Juvekar <mjuvekar7@gmail.com>
2018-07-03 22:46:00 +0530

    Implement clear button.

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function toggleGridCell() {
     counter = document.getElementById('weightCounter');
     weight = Number(counter.textContent);
-    if (this.classList.contains('gridCellOff')) {
-        this.className = 'gridCell gridCellOn';
-        counter.textContent = ++weight;
-    } else {
-        this.className = 'gridCell gridCellOff';
+    if (this.classList.contains('gridCellOn')) {
+        this.classList.remove('gridCellOn');
         counter.textContent = --weight;
+    } else {
+        this.classList.add('gridCellOn');
+        counter.textContent = ++weight;
     }
 }
 
@@ -19,7 +19,9 @@ grid = document.getElementById('designGrid');
 for (var i = 0; i < 12; i++) {
     for (var j = 0; j < 18; j++) {
         temp = document.createElement('div');
-        temp.className = 'gridCell gridCellOff';
+        temp.className = 'gridCell';
+        temp.setAttribute('data-row', i);
+        temp.setAttribute('data-col', j);
         temp.onclick = toggleGridCell;
         grid.appendChild(temp);
     }
@@ -28,4 +30,13 @@ for (var i = 0; i < 12; i++) {
 buttons = document.getElementsByTagName('button');
 for (var i = 0; i < buttons.length; i++) {
     buttons[i].onclick = styleClickedButton;
+}
+
+clearButton = document.getElementById('clearButton');
+clearButton.onclick = function () {
+    cells = [].slice.call(document.getElementsByClassName('gridCellOn'));
+    for (var i = 0; i < cells.length; i++) {
+        cells[i].classList.remove('gridCellOn');
+    }
+    document.getElementById('weightCounter').textContent = '0';
 }
